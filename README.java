e9322125c3b0e9ad6d6e5052cc78cbb326d2659f
abi123naya <connect2abinaya@gmail.com>
2016-07-08 11:47:42 +0530

    Update and rename README.md to README.java
    finish

---
+++ README.java
@@ -0,0 +1,20 @@
+# moon
+import.java.util.*;
+class Digit
+{ public static void main(String[] args)
+ { int n,c=0;
+   System.out.println("Enter the integer");
+   Scanner s=new Scanner(System.in);
+   n=s.nextInt();
+   while(n>0)
+    { n/10;
+      c++;
+    
+    }
+   
+ }
+
+
+
+
+]
